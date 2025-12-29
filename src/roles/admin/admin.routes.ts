3490eb468bda3190f6c39c654754c500747a0fe7
Varun Reddy <2464480@cognizant.com>
2025-12-29 10:55:51 +0530

    admin component

--- src/roles/admin/admin.routes.ts
+++ src/roles/admin/admin.routes.ts
@@ -2,15 +2,16 @@
 import { Routes } from "@angular/router";
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryListComponent } from "./category-list/category-list.component";
 
 export const adminRoutes: Routes = [
   {
     path: '',
-  component: AdminComponent,
-  children: [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'admin', redirectTo: 'dasboard', pathMatch: 'full' },
-    
-    ]
-  }
+    component: AdminComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'categories', component: CategoryListComponent },
+      { path: 'admin', redirectTo: 'dasboard', pathMatch: 'full' },
+    ],
+  },
 ];
